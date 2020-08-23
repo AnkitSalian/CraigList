d08a37bbc28a5e4c9ae8f8808671c04b4a42e36a
Ankit Salian <ankitsalian9@gmail.com>
2020-08-23 21:26:23 +0530

    Configured Craiglist frontend by live scraping

--- my_app/views.py
+++ my_app/views.py
@@ -1,14 +1,33 @@
 import requests
-from django.shortcuts import render
 from bs4 import BeautifulSoup
+from django.shortcuts import render
+from requests.compat import quote_plus
+from . import models
 
+BASE_CRAIGLIST_URL = 'https://mumbai.craigslist.org/search/?query={}'
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    trigger_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    response = requests.get(trigger_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listing = soup.find_all('li', {'class': 'result-row'})
+    final_postings = []
+    for post in post_listing:
+        title = post.find(class_='result-title')
+        post_title = 'Title Not Specified' if title is None else title.text
+        post_url = post.find('a').get('href')
+        price = post.find(class_='result-price')
+        post_price = 'Not Specified' if price is None else price.text
+        final_postings.append((post_title, post_url, post_price))
+
     stuff_for_frontend = {
-        'search': search
+        'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
